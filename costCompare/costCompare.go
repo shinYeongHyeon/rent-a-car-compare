be965fca1aaa5d664ba632b3b85574a2e9007870
shinYeongHyeon <den.shin.dev@gmail.com>
2021-01-12 23:17:05 +0900

    Implement JejuPass

--- costCompare/costCompare.go
+++ costCompare/costCompare.go
@@ -3,11 +3,11 @@ package costCompare
 import (
 	"context"
 	"fmt"
-	"github.com/chromedp/cdproto/cdp"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/chromedp"
 	"github.com/gin-gonic/gin"
-	"github.com/thedevsaddam/gojsonq"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -25,14 +25,19 @@ type JejuPassRequest struct {
 
 // CostCompare
 func CostCompare(c *gin.Context) {
+	start := c.Query("start")
+	end := c.Query("end")
+
 	contextVar, cancelFunc := chromedp.NewContext(
 		context.Background(),
 		chromedp.WithLogf(log.Printf),
 	)
+
 	defer cancelFunc()
 
 	var strVar string
-	var nodes []cdp.NodeID
+	costMaps := map[string]string{}
+	resultString := ""
 
 	contextVar, cancelFunc = context.WithTimeout(contextVar, 15*time.Second)
 	defer cancelFunc()
@@ -40,28 +45,33 @@ func CostCompare(c *gin.Context) {
 	err := chromedp.Run(contextVar,
 		chromedp.Navigate(`https://www.jejupassrent.com/home/search/list.do`),
 		chromedp.Click(`#YMD button`),
-		chromedp.Click(`.popover-content #date table tbody td[data-num="20210110"]`),
-		chromedp.Click(`.popover-content #date table tbody td[data-num="20210111"]`),
-		chromedp.Click(`.popover-content button.applyCalendarPanel`),
-		// 시간 생략 ... 귀찮아..
+		chromedp.Click(`.popover-content #date table tbody td[data-num="` + start + `"]`),
+		chromedp.Click(`.popover-content #date table tbody td[data-num="` + end + `"]`),
+		chromedp.Click(`.popover-content button.next.applyCalendarPanel`),
+		// TODO: 시간
 		chromedp.Click(`#container .popover-content .btn-wrap button.next`),
-		chromedp.NodeIDs(`#container #drawAreaView span.text-price`, &nodes, chromedp.BySearch),
-		chromedp.Text(nodes, &strVar, chromedp.ByNodeID),
-		//chromedp.Text(`#container #drawAreaView span.text-price`, &strVar, chromedp.ByQueryAll),
-		//chromedp.Text(`td[data-num]="20210110"`, &strVar, chromedp.NodeVisible, chromedp.ByQuery),
-		//chromedp.InnerHTML(`#popover399820`, &strVar, chromedp.NodeVisible, chromedp.ByQuery),
-		//chromedp.InnerHTML(`#container .popover-content .btn-wrap button.next`, &strVar, chromedp.NodeVisible, chromedp.ByQuery),
-		//chromedp.InnerHTML(`.popover-content td[data-num]="20210110"`, &strVar, chromedp.NodeVisible, chromedp.ByQuery),
+		chromedp.Click(`#container > div.content-wrap > div.sidebar > form#leftForm > div.search-car-input > div.btn-wrap > #searchBtn`),
+		chromedp.WaitVisible(`#content #drawAreaView`),
+		chromedp.InnerHTML(`#content #drawAreaView`, &strVar, chromedp.NodeVisible, chromedp.ByQuery),
 	)
 
 	if err != nil {
 		fmt.Println("Err : ", err)
 	}
-	fmt.Println(strVar)
 
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(strVar))
 
-}
+	if err != nil {
+		fmt.Println("Document Err : ", err)
+	}
 
-func getCarsAndCost(json *gojsonq.JSONQ) {
+	doc.Find("li.panel").Each(func (i int, s *goquery.Selection) {
+		model := s.Find("div.title > h3").Text()
+		cost := s.Find("div.right span.text-price").Text()
 
+		resultString += model + " : " + cost + "\n"
+		costMaps[model] = cost
+	})
+
+	c.String(200, resultString)
 }
